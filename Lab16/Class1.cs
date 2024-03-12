435fa8c2d6073696839ce2a83870ec882562c8cf
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-12 18:58:25 +0300

    Добавьте файлы проекта.

---
+++ Lab16/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Product
+    {
+        private string name;
+        private double price;
+        private double code;
+
+        public string Name { get { return name; } set { name = value; } } 
+        public double Price { get { return price; } set { price = value; } }
+        public double Code { get { return code; } set { code = value; } }
+
+        public Product(string name, double price, double code)
+        { 
+            Name = name;
+            Price = price;  
+            Code = code;    
+        }
+    }
+}
---
+++ Lab16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Product[] products = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите имя товара");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену товара");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите код товара");
+                double code = Convert.ToDouble(Console.ReadLine());
+
+                products[i] = new Product(name, price, code);
+
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true,
+            };
+
+
+            string jsonString =  JsonSerializer.Serialize(products,options);
+            using (StreamWriter sw = new StreamWriter("Products.json")) 
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
